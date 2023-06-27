74509a52596a6401c35ec75c651b88a2e16df0d6
aitnacer-nabil <118471449+aitnacer-nabil@users.noreply.github.com>
2023-06-27 11:44:24 +0100

    Init project steps

---
+++ steps/src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div class="steps">
+      <div class="numbers">
+        <div class="step-1">1</div>
+        <div class="step-2">2</div>
+        <div class="step-3">3</div>
+      </div>
+
+      <p class="message"></p>
+
+      <div class="buttons">
+        <button class="previous">Previous</button>
+        <button class="next">Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
